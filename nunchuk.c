ba87af64ca268c26d976dd20d82ab9dc9042db47
KubaTaba1uga <taba1uga6200@proton.me>
2024-05-28 07:25:17 +0000

    register button presses as input events

--- nunchuk.c
+++ nunchuk.c
@@ -15,7 +15,11 @@ static int nunchuk_probe(struct i2c_client *client,
                          const struct i2c_device_id *id);
 static void nunchuk_remove(struct i2c_client *client);
 static int nunchuk_init(struct i2c_client *client);
-static int nunchuk_read_register(struct i2c_client *client);
+int nunchuk_read_register(struct i2c_client *client, bool *is_z_pressed,
+                          bool *is_c_pressed);
+static int nunchuk_register_as_input_device(struct i2c_client *client,
+                                            struct input_dev **input_dev);
+static void nunchuk_poll(struct input_dev *input_dev);
 
 /* ########################################################### */
 /* #                    Public API                           # */
@@ -48,16 +52,12 @@ int nunchuk_probe(struct i2c_client *client, const struct i2c_device_id *id) {
 
   pr_info("%s\n", __func__);
 
-  input_dev_instance = devm_input_allocate_device(&client->dev);
-  if (!input_dev_instance)
-    return -ENOMEM;
-
   err = nunchuk_init(client);
   if (err) {
     return err;
   }
 
-  err = input_register_device(input_dev_instance);
+  err = nunchuk_register_as_input_device(client, &input_dev_instance);
   if (err) {
     return err;
   }
@@ -94,7 +94,69 @@ int nunchuk_init(struct i2c_client *client) {
   return 0;
 };
 
-int nunchuk_read_register(struct i2c_client *client) {
+int nunchuk_register_as_input_device(struct i2c_client *client,
+                                     struct input_dev **input_dev) {
+  struct input_dev *input_dev_placeholder;
+  int err;
+
+  pr_info("%s\n", __func__);
+
+  /* Allocate input device instance */
+  input_dev_placeholder = devm_input_allocate_device(&client->dev);
+  if (!input_dev) {
+    pr_err("Unable to allocate nunchuk's input device\n");
+    return -ENOMEM;
+  }
+
+  /* Input device configuration */
+  ///* Driver metadata */
+  input_dev_placeholder->name = "Wii Nunchuk";
+  input_dev_placeholder->id.bustype = BUS_I2C;
+
+  ///* User space key mapping */
+  set_bit(EV_KEY, input_dev_placeholder->evbit);
+  set_bit(BTN_C, input_dev_placeholder->keybit);
+  set_bit(BTN_Z, input_dev_placeholder->keybit);
+
+  ///* Link i2c_client to input_dev_placeholder */
+  input_set_drvdata(input_dev_placeholder, client);
+
+  ///* Set up polling */
+  input_setup_polling(input_dev_placeholder, nunchuk_poll);
+  input_set_poll_interval(input_dev_placeholder, 50);
+
+  /* Register nunchuk as input device */
+  err = input_register_device(input_dev_placeholder);
+  if (err) {
+    pr_err("Unable to register nunchuk as input device\n");
+    return err;
+  }
+
+  *input_dev = input_dev_placeholder;
+
+  return 0;
+}
+
+void nunchuk_poll(struct input_dev *input_dev) {
+  struct i2c_client *client = input_get_drvdata(input_dev);
+  bool is_z_pressed, is_c_pressed;
+  int err;
+
+  err = nunchuk_read_register(client, &is_z_pressed, &is_c_pressed);
+  if (err) {
+    pr_err("Unable to poll the nunchuk\n");
+    return;
+  }
+
+  input_report_key(input_dev, BTN_Z, is_z_pressed);
+  input_report_key(input_dev, BTN_C, is_c_pressed);
+  input_sync(input_dev);
+
+  return;
+}
+
+int nunchuk_read_register(struct i2c_client *client, bool *is_z_pressed,
+                          bool *is_c_pressed) {
   char buf[6] = {0x00}; // We just reusing one buffer for all operations
   int bytes_amount;
 
@@ -118,14 +180,17 @@ int nunchuk_read_register(struct i2c_client *client) {
     return -2;
   }
 
-  /* For 6 byte of read value: */
+  /* For 6'th byte of read value: */
   /* • bit 0 == 0 means that Z is pressed. */
   /* • bit 0 == 1 means that Z is released. */
   /* • bit 1 == 0 means that C is pressed. */
   /* • bit 1 == 1 means that C is released. */
-  pr_info("Z button (%s), C button (%s)\n",
-          (buf[5] & BIT(0)) == 0 ? "Pressed" : "Released",
-          (buf[5] & BIT(1)) == 0 ? "Pressed" : "Released");
+  *is_z_pressed = (buf[5] & BIT(0)) == 0;
+  *is_c_pressed = (buf[5] & BIT(1)) == 0;
+
+  /* pr_info("Z button (%s), C button (%s)\n", */
+  /*         is_z_pressed ? "Pressed" : "Released", */
+  /*         is_c_pressed ? "Pressed" : "Released"); */
 
   return 0;
 }
